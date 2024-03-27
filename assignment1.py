fa482ed4ab206382c234707111a488c4f06ab405
raniakhaled15 <you@example.com>
2024-03-27 02:04:46 +0200

    Create main.py

--- assignment1.py
+++ assignment1.py
@@ -21,6 +21,65 @@ def eliminate_implication(expression):
     return new_expression
 
 
-expression = "∀x∀y∃z(p(x)  ~r(y)) ⇒  q(z)"
+def apply_demorgan(expression):
+    if '∧' in expression:
+        parts = expression.split('∧')
+        expression = '(~' + parts[0].strip() + ' ∨ (~' + parts[1].strip() + ')'
+
+    elif '∨' in expression:
+        parts = expression.split('∨')
+        expression = '(~' + parts[0].strip() + ' ∧ (~' + parts[1].strip() + ')'
+    return expression
+
+
+def get_inner_part(idx, expression):
+    stack = ['(']
+    new_expression = ""
+    while len(stack) != 0:
+        # new_expression += expression[idx]
+        if expression[idx] == '(':
+            stack.append('(')
+        elif expression[idx] == ')':
+            stack.pop()
+        new_expression += expression[idx]
+        idx += 1
+    return new_expression, idx
+
+
+def move_negation_inward(sentence):
+    """Apply DeMorgan's Law to move negation inward"""
+    if "~(" in sentence:
+        negation_index = sentence.index("~(")
+        # closing_index = negation_index + sentence[negation_index:].index(")")
+        inner_part, closing_index = get_inner_part(negation_index + 2, sentence)
+        inner_part = apply_demorgan(inner_part)
+        inner_part = move_negation_inward(inner_part)  # Recursively move inward
+        return sentence[:negation_index] + inner_part + sentence[closing_index + 1:]
+    return sentence
+
+
+def remove_double_negation(expression):
+    new_expression = expression.replace("~~", "")
+    return new_expression
+
+
+def delete_universal_quantifiers(expression):
+    expression = expression.replace("∀x", "")
+    expression = expression.replace("∀y", "")
+    expression = expression.replace("∀z", "")
+    expression = expression.replace("∃x", "")
+    expression = expression.replace("∃y", "")
+    expression = expression.replace("∃z", "")
+    return expression
+
+
+expression = "∀x∀y(p(x) ∧ ~r(y)) ⇒  ∃zq(z)"
 print("Original expression:", expression)
-print("Step 1(Eliminate implication):", eliminate_implication(expression))
+expression = eliminate_implication(expression)
+print("Step 1 and 5(Eliminate implication):\n", expression)
+expression = move_negation_inward(expression)
+print("Step 2(Move negation inward (Demorgan Law))\n:", expression)
+expression = remove_double_negation(expression)
+print("Step 3(Remove double-not)\n:", expression)
+expression = delete_universal_quantifiers(expression)
+print("Step 7(Eliminate universal quantifiers)\n:", expression)
